ab7f3e3958363d3e93e8188aff009a12188748e3
Laurence Castillo <laurence.castillo@me.com>
2025-07-15 17:06:10 -0400

    starting routes_auth.py

--- app/api/routes_auth.py
+++ app/api/routes_auth.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {'message' : 'yo yo testing'}
+
+@app.get("/login")
+async def get_login():
+    pass
+@app.post("login")
+async def post_login():
+    pass
+@app.get("/homepage")
+async def home():
+    pass
