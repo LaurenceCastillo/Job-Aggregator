c00765fbecb7060a6c34e70169e5fee9b0947f8a
Laurence Castillo <laurence.castillo@me.com>
2025-07-27 17:56:13 -0400

    idk what i'm committing

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -3,13 +3,15 @@ from starlette.middleware.cors import CORSMiddleware
  
 from app.api import router as api_router
 
+from app.core import config, tasks
+
 #from app.api.routes import router as api_router
 from app.api import routes_auth
 from app.api import routes_jobs
 from app.api import routes_users
  
 def get_application():
-    app = FastAPI(title="Phresh", version="1.0.0")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
  
     app.add_middleware(
         CORSMiddleware,
@@ -18,11 +20,12 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+
+    app.add_event_handler("startup",tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown",tasks.create_stop_app_handler(app))
  
     app.include_router(api_router, prefix="/api")
-    #app.include_router(routes_auth, prefix = "/api")
-    #app.include_router(routes_jobs, prefix = "/api")
-    #app.include_router(routes_users, prefix = "/api")
 
     return app
  
